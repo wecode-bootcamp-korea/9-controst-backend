a4e90679cc3f22018520fc50764e6829633cba54
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-06-25 15:08:43 +0900

    [Add] partner app started, created classes in partner.models.py, set manytomanyfield, made migrations and migrate . created db_uploader.py and csv file where manage.py is in.

--- db_uploader.py
+++ db_uploader.py
@@ -3,13 +3,29 @@ import django
 import csv
 import sys
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trost.settings")
-#django.setup()
-#from partner.models import Counselor, Theme, Counselor_Theme, Counsel_Kind, Counselor_Counsel_kind, Duration, Product
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trost.settings")
+django.setup()
+#from partner.models import Counselor, Level, Theme, CounselKind, Duration, Product
 
-#CSV_PATH_COUNSELORS = './trost_crwaling_1(csv).csv'
+CSV_PATH_COUNSELORS = './trost_crawling_1.csv'
 
 #with open(CSV_PATH_COUNSELORS) as in_file:
 #    data_reader = csv.reader(in_file)
 #    next(data_reader, None)
-#    for row in data_reader:
+#    level = []
+   # for row in data_reader:
+  #      name = row[0]
+ #       gender =row[2]
+#        counsel_count = row[4]
+#        introduction = row[5]
+
+#        print(name, gender, counsel_count, introduction)
+
+        ###level into DB###
+#        levels = row[1]
+#        if levels not in level:
+#            level.append(levels)
+#    for i in level:
+#        Level.objects.create(level=i)
+
+
--- partner/models.py
+++ partner/models.py
@@ -7,48 +7,34 @@ class Counselor(models.Model):
 
     name = models.CharField(max_length=50)
     gender = models.IntegerField(choices=GENDER_CHOICES)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    level = models.ForeignKey('Level', on_delete=models.CASCADE)
     counsel_count = models.IntegerField(default=0)
     introduction = models.CharField(max_length=1000)
     is_counsel_gt_150 = models.IntegerField(default=0)
     profile_image_url = models.URLField(max_length=200)
-    counsel_themes = models.ManyToManyField(Theme)
-    counsel_kinds = models.ManyToManyField(Counsel_Kind)
+    counsel_themes = models.ManyToManyField('Theme')
+    counsel_kinds = models.ManyToManyField('CounselKind')
 
     class Meta:
         db_table = "counselors"
 
 class Level(models.Model):
-    level = models.CharFIeld(max_length=20)
+    level = models.CharField(max_length=19)
 
     class Meta:
         db_table = "levels"
 
 class Theme(models.Model):
-    theme = models.CharField(max_length=10)
+    theme = models.CharField(max_length=9)
 
     class Meta:
         db_table = "themes"
 
-class Counselor_Theme(models.Model):
-    counselor = models.ForeignKey(Counselor, on_delete=models.CASCADE)
-    counsel_theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "counselors_themes"
-
-class Counsel_Kind(models.Model):
+class CounselKind(models.Model):
     counsel_kind = models.CharField(max_length=20)
 
     class Meta:
         db_table = "counsel_kinds"
-
-class Counselor_Counsel_kind(models.Model):
-    counselor = models.ForeignKey(Counselor, on_delete=models.CASCADE)
-    counsel_kind = models.ForeignKey(Counsel_Kind, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = "counselors_counsel_kinds"
 
 class Duration(models.Model):
     duration = models.CharField(max_length=30)
@@ -56,11 +42,11 @@ class Duration(models.Model):
     class Meta:
         db_table = "durations"
 
-class product(models.Model):
+class Product(models.Model):
     level = models.ForeignKey(Level, on_delete = models.CASCADE)
-    counsel_kind = models.ForeignKey(Counsel_Kind, on_delete=models.CASCADE)
+    counsel_kind = models.ForeignKey(CounselKind, on_delete=models.CASCADE)
     duration = models.ForeignKey(Duration, on_delete=models.CASCADE)
-    price = models.IntegerField(deafult=0)
+    price = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'products'
