3d5a13a23f7f77214627bb10ba1cb37594ef4fc6
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-06-26 11:08:29 +0900

    [Add] partner app started, created classes in partner.models.py, made migrations and migrate , model arranged

--- partner/models.py
+++ partner/models.py
@@ -1,34 +1,34 @@
 from django.db import models
 
 class Counselor(models.Model):
-    name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=10)
-    level = models.ForeignKey('Level', on_delete=models.CASCADE)
-    counsel_count = models.IntegerField(default=0)
-    introduction = models.CharField(max_length=1000)
+    name              = models.CharField(max_length=50)
+    gender            = models.CharField(max_length=10)
+    level             = models.ForeignKey('Level', on_delete=models.CASCADE)
+    counsel_count     = models.IntegerField(default=0)
+    introduction      = models.CharField(max_length=1000)
     is_counsel_gt_150 = models.IntegerField(default=0)
     profile_image_url = models.URLField(max_length=2000)
-    theme = models.ManyToManyField('Theme', through = 'CounselorTheme')
-    kind = models.ManyToManyField('Kind', through = 'CounselorKind')
+    theme             = models.ManyToManyField('Theme', through = 'CounselorTheme')
+    kind              = models.ManyToManyField('Kind', through = 'CounselorKind')
 
     class Meta:
         db_table = "counselors"
 
 class Level(models.Model):
-    name = models.CharField(max_length=19)
+    name = models.CharField(max_length=20)
 
     class Meta:
         db_table = "levels"
 
 class Theme(models.Model):
-    name = models.CharField(max_length=9)
+    name = models.CharField(max_length=10)
 
     class Meta:
         db_table = "themes"
 
 class CounselorTheme(models.Model):
     counselor = models.ForeignKey('Counselor', on_delete=models.CASCADE)
-    theme = models.ForeignKey('Theme', on_delete=models.CASCADE)
+    theme     = models.ForeignKey('Theme', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'counselor_themes'
@@ -41,7 +41,7 @@ class Kind(models.Model):
 
 class CounselorKind(models.Model):
     counselor = models.ForeignKey('Counselor', on_delete=models.CASCADE)
-    kind = models.ForeignKey('Kind', on_delete=models.CASCADE)
+    kind      = models.ForeignKey('Kind', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'counselor_kinds'
@@ -53,10 +53,10 @@ class Duration(models.Model):
         db_table = "durations"
 
 class Product(models.Model):
-    level = models.ForeignKey('Level', on_delete = models.CASCADE)
-    kind = models.ForeignKey('Kind', on_delete=models.CASCADE)
+    level    = models.ForeignKey('Level', on_delete = models.CASCADE)
+    kind     = models.ForeignKey('Kind', on_delete=models.CASCADE)
     duration = models.ForeignKey('Duration', on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
+    price    = models.DecimalField(max_digits=12, decimal_places=2)
 
     class Meta:
         db_table = 'products'
--- partner/views.py
+++ partner/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import(
+        JsonResponse,
+        HttpResponse
+        )
+
+from .models import Counselor, Level, Theme, CounselorTheme, Kind, CounselorKind, Duration, Product
+
+class Partner(View):
+    def get(self, request):
+        partner_list = [
+                {'name':
+                 'img':
+                 'level':
+                 'introduction':
+                 }
+                
+                ]
+        return JsonResponse
