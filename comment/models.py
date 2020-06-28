35760d401827a6fd91e65c76e6a1311395118d6b
SeongwonMoon <swmarkmoon@gmail.com>
2020-06-28 18:09:43 +0900

    [Add] Create model in comment app

---
+++ comment/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from partner.models import Counselor, Theme
+from user.models import History
+
+class Review(models.Model):
+    history = models.ForeignKey('History', on_delete = models.PROTECT)
+    created_at =  models.DateTimeField(auto_now_add = True)
+    score = models.FloatField(min_value=0.0, max_value=5.0, decimal_places=1, defualt=2.5)
+    comment = models.CharField(max_length=10000)
+    theme = models.ManytoManyField('Theme', through='ReviewThemes', related_name = 'review') 
+
+    class Meta:
+        db_table = 'reviews'
+
+class ReviewTheme(models.Model):
+    review = models.ForeignKey('Review', on_delete = models.CASCADE)
+    theme = models.ForiengKey('Themes', on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'reviews_themes'
