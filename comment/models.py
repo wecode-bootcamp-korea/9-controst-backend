85f7131007e01278067a7df8beafee355f5b9b31
SeongwonMoon <swmarkmoon@gmail.com>
2020-06-28 19:12:03 +0900

    [Fix] Typos

--- comment/models.py
+++ comment/models.py
@@ -5,16 +5,16 @@ from users.models import History
 class Review(models.Model):
     history = models.ForeignKey('History', on_delete = models.PROTECT)
     created_at =  models.DateTimeField(auto_now_add = True)
-    score = models.FloatField(defualt=2.5)
+    score = models.FloatField(default=2.5)
     comment = models.CharField(max_length=10000)
-    theme = models.ManytoManyField('Theme', through='ReviewThemes', related_name = 'review') 
+    theme = models.ManyToManyField('Theme', through='ReviewThemes', related_name = 'review') 
 
     class Meta:
         db_table = 'reviews'
 
 class ReviewTheme(models.Model):
     review = models.ForeignKey('Review', on_delete = models.CASCADE)
-    theme = models.ForiengKey('Themes', on_delete = models.CASCADE)
+    theme = models.ForeignKey('Themes', on_delete = models.CASCADE)
 
     class Meta:
         db_table = 'reviews_themes'
