2ba7319d5b645fdc9d1e19a26d95648097fa2dda
SeongwonMoon <swmarkmoon@gmail.com>
2020-06-28 19:50:40 +0900

    [Fix] Apply Convention

--- comment/models.py
+++ comment/models.py
@@ -1,20 +1,18 @@
 from django.db import models
-from partner.models import Counselor, Theme
-from users.models import History
 
 class Review(models.Model):
-    history = models.ForeignKey('History', on_delete = models.PROTECT)
+    history    = models.ForeignKey('users.History', on_delete = models.PROTECT)
     created_at =  models.DateTimeField(auto_now_add = True)
-    score = models.FloatField(default=2.5)
-    comment = models.CharField(max_length=10000)
-    theme = models.ManyToManyField('Theme', through='ReviewThemes', related_name = 'review') 
+    score      = models.FloatField(default=2.5)
+    comment    = models.CharField(max_length=10000)
+    theme      = models.ManyToManyField('partner.Theme', through='ReviewTheme', related_name = 'review') 
 
     class Meta:
         db_table = 'reviews'
 
 class ReviewTheme(models.Model):
     review = models.ForeignKey('Review', on_delete = models.CASCADE)
-    theme = models.ForeignKey('Themes', on_delete = models.CASCADE)
+    theme  = models.ForeignKey('partner.Theme', on_delete = models.CASCADE)
 
     class Meta:
         db_table = 'reviews_themes'
