4ae353aa6719c93eec456202a747e74cf85c677f
SeongwonMoon <swmarkmoon@gmail.com>
2020-06-29 14:11:15 +0900

    [Fix] delelted unnecessary line and applied convention

--- comment/models.py
+++ comment/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Review(models.Model):
     history    = models.ForeignKey('users.History', on_delete = models.PROTECT)
-    created_at =  models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add = True)
     score      = models.FloatField(default=2.5)
     comment    = models.CharField(max_length=10000)
     theme      = models.ManyToManyField('partner.Theme', through='ReviewTheme', related_name = 'review') 
