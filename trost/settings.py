1c947d76954d570674296f52dab6e27cc981d1f0
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-06-29 15:14:36 +0900

    [Add]partner, ListUpview 추가

--- trost/settings.py
+++ trost/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = my_settings.SECRET['secret']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',"10.58.7.28", "10.58.7.28:8000"]
 
 
 # Application definition
