812856154afd88c39b2734cf1a79b0b20b236df8
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-06-30 19:44:30 +0900

    [Fix] feedback applied, new class in models.py created('CounselorContent')

--- trost/urls.py
+++ trost/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import (
-        path,
-        include)
+    path,
+    include
+)
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
    path('partner', include('partner.urls')),
 ]
