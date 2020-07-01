ccb0a2877367a0a20d1acabba58280c87b9cfa0c
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-07-01 16:47:14 +0900

    map app started

--- map/models.py
+++ map/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
+class CounselCenter(models.Model):
+    longitude = models.DecimalField(max_digits=22, decimal_places=16)
+    latitude = models.DecimalField(max_digits=22, decimal_places=16)
+    center_type = models.CharField(max_length=20)
+    name        = models.CharField(max_length=50)
+    homepage_url = models.CharField(max_length=2000)
+    phone_number = models.CharField(max_length=20)
+    address      = models.CharField(max_length=1000)
+    road_address = models.CharField(max_length=1000)
+    is_trost_partner = models.BooleanField(default=False)
+    partner_info     = models.CharField(max_length=2000)
+    counseling_type  = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'counselcenters'
 
 # Create your models here.
