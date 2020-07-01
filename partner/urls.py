912e039f3cd0f7ba361f87fecfa5422211aa3808
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-07-01 16:09:19 +0900

    PartnerDetailView completed, partner.urls added

--- partner/urls.py
+++ partner/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import CounselorListView
+from .views import (
+        CounselorListView,
+        CounselorDetailView
+        )
 
 urlpatterns = [
     path('', CounselorListView.as_view()),
+    path('/<slug:partner_id>', CounselorDetailView.as_view())
 ]
 
 
