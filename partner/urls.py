bda97d7b2b3e014cfed019cdb2416fcd7c1a7cd1
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-07-01 17:07:08 +0900

    blank deleted

--- partner/urls.py
+++ partner/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from .views import (
         CounselorListView,
-        CounselorDetailView
         )
 
 urlpatterns = [
     path('', CounselorListView.as_view()),
-    path('/<slug:partner_id>', CounselorDetailView.as_view())
 ]
-
-
