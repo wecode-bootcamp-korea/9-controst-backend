d099fa42cd627b305aba68773cb1396c12a35334
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-07-02 22:30:41 +0900

    [Add]map app started, map.model made, map.view made, map.url made,  CounselorDetailView in partner.views made

--- map/views.py
+++ map/views.py
@@ -9,25 +9,23 @@ from .models import (
 
 class MapView(View):
     def get(self, request):
-        location = []
+        locations = []
         centers = CounselCenter.objects.all()
         for center in centers:
-            location.append(
+            locations.append(
                 {
-                    "mapX": center.longitude,
-                    "mapY": center.latitude,
-                    "type": center.center_type,
-                    "title": center.name,
-                    "link": center.homepage_url,
-                    "telephone": center.phone_number,
-                    "address": center.address,
-                    "roadAddress": center.road_address,
-                    "isTrostPartnerCenter": center.is_trost_partner,
-                    "trostPartners": center.partner_info,
-                    "counselingTypes": center.counseling_type
+                    "mapX"                 : center.longitude,
+                    "mapY"                 : center.latitude,
+                    "type"                 : center.center_type,
+                    "title"                : center.name,
+                    "link"                 : center.homepage_url,
+                    "telephone"            : center.phone_number,
+                    "address"              : center.address,
+                    "roadAddress"          : center.road_address,
+                    "isTrostPartnerCenter" : center.is_trost_partner,
+                    "trostPartners"        : eval(center.partner_info),
+                    "counselingTypes"      : eval(center.counseling_type)
                     }
-)
+        )
 
-
-        return JsonResponse({"location":location}, status=200)
-# Create your views here.
+        return JsonResponse({"location":locations}, status=200)
--- partner/views.py
+++ partner/views.py
@@ -21,14 +21,18 @@ from comment.models import Review
 class CounselorListView(View):
     def get(self, request):
         partners_list = []
-        partners = Counselor.objects.all()
+        offset        = request.GET.get('offset', 1)
+        limit         = request.GET.get('limit', 12)
+        partners = Counselor.objects.prefetch_related('history_set').prefetch_related('history_set__review_set')[offset:limit]
+        reviews  = Review.objects.select_related('history').all()
+        products = Product.objects.select_related('level').all()
         for partner in partners:
             stars        = partner.history_set.aggregate(Avg('review__score'))
-            review_count = Review.objects.filter(history__in=partner.history_set.all()).count()
-            prices       = Product.objects.filter(level=partner.level)
-            prices_list  = [price.price for price in prices]
+            review_count = reviews.filter(history__in=partner.history_set.all()).count()
+            prices  = [price.price for price in products.filter(level=partner.level)]
             partners_list.append(
                     {
+                        "partner_id"             : partner.id,
                         "name"                   : partner.name, 
                         "gender"                 : partner.gender, 
                         "level"                  : partner.level.name, 
@@ -36,7 +40,7 @@ class CounselorListView(View):
                         "introduction"           : partner.introduction, 
                         "is_cousel_count_gt_150" : partner.is_counsel_count_gt_150, 
                         "profile_image_url"      : partner.profile_image_url, 
-                        "prices"                 : prices_list,
+                        "prices"                 : prices,
                         "stars"                  : stars,
                         "review_count"           : review_count
                      }
@@ -55,40 +59,39 @@ class CounselorDetailView(View):
          review_count = Review.objects.filter(history__in=partner.history_set.all()).count()
          partner_detail.append(
                  {
-                     "partner_name" : partner.name,
-                     "partner_level": partner.level.name,
-                     "counsel_count" : partner.counsel_count,
+                     "partner_name"           : partner.name,
+                     "partner_level"          : partner.level.name,
+                     "counsel_count"          : partner.counsel_count,
                      "is_cousel_count_gt_150" : partner.is_counsel_count_gt_150,
-                     "profile_image_url" : partner.profile_image_url,
-                     "prices" : prices_list,
-                     "stars": stars,
-                     "review_count": review_count,
+                     "profile_image_url"      : partner.profile_image_url,
+                     "prices"                 : prices_list,
+                     "stars"                  : stars,
+                     "review_count"           : review_count,
 
                  }
-             )
+          )
 
          content = CounselorContent.objects.get(counselor=partner_id).name
          partner_detail.append(
                  {
-                     "patner_content" : content
+                     "patner_content" : eval(content)
                  }
              )
 
          counselor_histories = Counselor.objects.prefetch_related('history_set').get(id=partner_id).history_set.all()
-         counselor_reviews = Review.objects.filter(history__in=counselor_histories)
-         reviews_list = []
+         counselor_reviews   = Review.objects.filter(history__in=counselor_histories)
+         reviews_list        = []
          for i in counselor_reviews:
              reviews_list.append(
                  {
-                     'review_created': i.created_at,
-                     'review_score': i.score,
-                     'review_comment': i.comment
+                     'review_created' : i.created_at,
+                     'review_score'   : i.score,
+                     'review_comment' : i.comment
                  }
              )
          partner_detail.append(
                  {
-                 "reviews":reviews_list
+                 "reviews" : reviews_list
                  }
-             )
-
+          )
          return JsonResponse({"information":partner_detail}, status=200)
