d8cf7b0d7d6bfb6d7eb9da6a1682e98aa61f58cc
jayden <guitarlove00@gmail.com>
2020-07-03 13:57:00 +0900

    [Fix] list comprehension in HistoryView, put HS256 in my_settings.py and refactoring

--- user/views.py
+++ user/views.py
@@ -8,7 +8,7 @@ from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 from django.views import View
 
-from trost.settings import SECRET_KEY
+from trost.settings import SECRET_KEY, HASH_KEY
 from partner.models import (
         Counselor,
         Product
@@ -28,63 +28,47 @@ class SignUpView(View):
         # regex of password validation
         psw_validator = RegexValidator(regex='^.*(?=^.{6,14}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*?()_~]).*$')
 
-        # the button of signup
-        if 'email' in data.keys() and 'password' in data.keys() and 'nickname' in data.keys():
-            # email address validation
-            try:
-                email_validator(data['email'])
-            except ValidationError:
-                return JsonResponse({'message':'Invalid email'}, status= 400)
-            # check email duplication
-            else:
-                if User.objects.filter(email=data['email']).exists():
-                    return JsonResponse({'message':'이미 사용 중인 이메일 입니다.'}, status=400)
-                # password validation
-                else:
-                    try:
-                        psw_validator(data['password'])
-                    except ValidationError:
-                        return JsonResponse({'message':'Invalid password'}, status= 400)
-                    else:
-                        enc_pw=bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+        try:
+            email_validator(data['email'])
+            if User.objects.filter(email=data['email']).exists():
+                return HttpResponse(status=400)
+            psw_validator(data['password'])
+            enc_pw=bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+            final_nickname = data['nickname']
+            if not data['nickname']:
+                # random nickname generator
+                final_nickname = names.get_first_name()
+            # saving data
+            User(
+                email    = data['email'],
+                password = enc_pw.decode('utf-8'),
+                nickname = final_nickname,
+            ).save()
+        except ValidationError:
+            return HttpResponse(status= 400)
+        else:
+            return JsonResponse({'message':'SUCCESS'}, status=200)
 
-                        # random nickname generator
-                        if data['nickname'] != '':
-                            final_nickname = data['nickname']
-                        else:
-                            final_nickname = names.get_first_name()
+# the button of checking email duplication
+class EmailCheckView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        if User.objects.filter(email=data['email']).exists():
+            return HttpResponse(status= 400)
+        else:
+            return HttpResponse(status= 200)
 
-                        # saving data
-                        User(
-                            email    = data['email'],
-                            password = enc_pw.decode('utf-8'),
-                            nickname = final_nickname,
-                        ).save()
-                        return JsonResponse({'message':'SUCCESS'}, status=200)
-
-        # the button of checking email duplication
-        elif 'email' in data.keys() and 'password' not in data.keys() and 'nickname' not in data.keys():
-            try:
-                email_validator(data['email'])
-            except ValidationError:
-                return JsonResponse({'message':'Invalid email'}, status= 400)
-            except ValidationError:
-                return JsonResponse({'message':'Invalid Key'}, status= 400)
-            else:
-                if User.objects.filter(email=data['email']).exists():
-                    return JsonResponse({'message':'이미 사용 중인 이메일 입니다.'}, status= 400)
-                else:
-                    return JsonResponse({'message':'사용 가능한 이메일 입니다.'}, status= 200)
-
-        # the button of checking nickname duplication
-        elif 'email' not in data.keys() and 'password' not in data.keys() and 'nickname' in data.keys():
-            if User.objects.filter(nickname=data['nickname']).exists():
-                return JsonResponse({'message':'이미 사용 중인 닉네임입니다.'}, status= 400)
-            else:
-                return JsonResponse({'message':'사용 가능한 닉네임입니다.'}, status= 200)
+# the button of checking nickname duplication
+class NicknameCheckView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        if User.objects.filter(nickname=data['nickname']).exists():
+            return HttpResponse(status= 400)
+        else:
+            return HttpResponse(status= 200)
 
     def get(self, request):
-        return JsonResponse({"message":"success"}, status=200)
+        return HttpResponse(status=200)
 
 class SignInView(View):
     def post(self, request):
@@ -94,21 +78,21 @@ class SignInView(View):
                 user_info = User.objects.get(email=data['email'])
                 # password decrypting
                 if bcrypt.checkpw(data['password'].encode('utf-8'), user_info.password.encode('utf-8')):
-                    token=jwt.encode({"email":user_info.email}, SECRET_KEY, algorithm='HS256').decode('utf-8')
+                    token=jwt.encode({"email":user_info.email}, SECRET_KEY, algorithm=HASH_KEY).decode('utf-8')
                     return JsonResponse({"token":token}, status = 200)
-                return JsonResponse({"message":"계정 또는 비밀번호가 일치하지 않습니다."}, status = 401)
-            return JsonResponse({'message':'계정 또는 비밀번호가 일치하지 않습니다.'}, status = 400)
+                return HttpResponse(status = 401)
+            return HttpResponse(status = 400)
         except KeyError:
             return jsonResponse({'message': 'Invalid_KEY'})
 
     def get(self, request):
-        return JsonResponse({"message":"success"}, status=200)
+        return HttpResponse(status=200)
 
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             token = request.headers.get('Authorization', None)
-            payload = jwt.decode(token, SECRET_KEY, algorithm='HS256')
+            payload = jwt.decode(token, SECRET_KEY, algorithm=HASH_KEY)
             user_mail = User.objects.get(email=payload['email'])
             request.user = user_mail
         except jwt.exceptions.DecodeError:
@@ -122,17 +106,16 @@ class HistoryView(View):
     @login_decorator
     def get(self, request):
         account = request.user
-        user_info = History.objects.filter(user__email = account.email)
-        history_list = []
-        for i in range(len(user_info)):
-            history_list.append(
+        user_info = History.objects.filter(user__email = account.email).select_related('counselor').select_related('product')
+        history_list = [
                 {
-                "counselor_name":user_info[i].counselor.name,
-                "counselor_level":user_info[i].counselor.level.name,
-                "profile_image_url":user_info[i].counselor.profile_image_url,
-                "product_kind":user_info[i].product.kind.name,
-                "product_duration":user_info[i].product.duration.name,
-                "product_price":user_info[i].product.price,
-                "counsel_date":user_info[i].created_at
-                })
+                "counselor_name"    : info.counselor.name,
+                "counselor_level"   : info.counselor.level.name,
+                "profile_image_url" : info.counselor.profile_image_url,
+                "product_kind"      : info.product.kind.name,
+                "product_duration"  : info.product.duration.name,
+                "product_price"     : info.product.price,
+                "counsel_date"      : info.created_at
+                } for info in user_info
+                ]
         return JsonResponse({"user_history":history_list}, status=200)
