9423943d18c27d5da52a63b2e6e572b49058bc86
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-07-03 14:54:40 +0900

    [Refactoring] pagination map views )

--- map/views.py
+++ map/views.py
@@ -2,17 +2,16 @@ import json
 
 from django.views import View
 from django.http import JsonResponse
-#from django.core.paginator import Paginator
 from .models import (
        CounselCenter 
 )
 
 class MapView(View):
     def get(self, request):
-        centers = CounselCenter.objects.all()
-        #paginator = Paginator(centers,20)
-        #page = request.GET.get('page', 1)
-        #location = paginator.page(page)
+        page    = request.GET.get('page', 1)
+        limit   = 20*int(page)
+        offset  = limit-20
+        centers = CounselCenter.objects.all()[offset:limit]
 
         locations = [ 
                 {
@@ -30,9 +29,5 @@ class MapView(View):
                     }
                 for center in centers
         ]
-        #paginator = Paginator(locations,20)
-        #page = request.GET.get('page',2)
-        #print(page)
-        #location = paginator.page(page)
 
         return JsonResponse({"location":locations}, status=200)
