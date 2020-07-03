010fcb6a9b5e1dd1c486656cf924a53bf203e5ce
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-07-03 11:51:54 +0900

    [Refactoring] partner.views review_results

--- partner/views.py
+++ partner/views.py
@@ -2,8 +2,11 @@ import json
 
 from django.views import View
 from django.http import JsonResponse
-from django.db.models import Avg
-
+from django.db.models import (
+        Avg,
+        Count,
+        F
+)
 from .models import (
     Counselor,
     Level,
@@ -20,15 +23,13 @@ from comment.models import Review
 
 class CounselorListView(View):
     def get(self, request):
-        offset        = request.GET.get('offset', 1)
-        limit         = request.GET.get('limit', 12)
-        partners = Counselor.objects.prefetch_related('history_set').prefetch_related('history_set__review_set')[offset:limit]
-        reviews  = Review.objects.select_related('history').all()
-        products = Product.objects.select_related('level').all()
-        partners_list = []
-        for partner in partners:
-            stars        = partner.history_set.aggregate(Avg('review__score'))
-            review_count = reviews.filter(history__in=partner.history_set.all()).count()
+        offset         = request.GET.get('offset', 1)
+        limit          = request.GET.get('limit', 12)
+        partners       = Counselor.objects.prefetch_related('history_set').prefetch_related('history_set__review_set')[offset:limit]
+        products       = Product.objects.select_related('level').all()
+        review_results = partners.values('name').annotate(review_score=Avg('history__review__score'), review_count= Count('history__review'))
+        partners_list  = []
+        for index, partner in enumerate(partners):
             prices  = [price.price for price in products.filter(level=partner.level)]
             partners_list.append(
                     {
@@ -41,8 +42,8 @@ class CounselorListView(View):
                         "is_cousel_count_gt_150" : partner.is_counsel_count_gt_150, 
                         "profile_image_url"      : partner.profile_image_url, 
                         "prices"                 : prices,
-                        "stars"                  : stars,
-                        "review_count"           : review_count
+                        "stars"                  : review_results[index]['review_score'], 
+                        "review_count"           : review_results[index]['review_count']
                      }
                 )
 
@@ -82,9 +83,9 @@ class CounselorDetailView(View):
          counselor_reviews   = Review.objects.filter(history__in=counselor_histories)
          reviews_list        = [
                  {
-                     'review_created' : i.created_at,
-                     'review_score'   : i.score,
-                     'review_comment' : i.comment
+                     'review_created' : review.created_at,
+                     'review_score'   : review.score,
+                     'review_comment' : review.comment
                  } for review in counselor_reviews
             ]
 
